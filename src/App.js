cf479e57d77f81ea7860f96c3af8c913bae41b0a
Amos Calamida <amos.calamida@me.com>
2023-10-11 13:15:23 +0000

    Initialize Plugins in Dev Container + Add simple intro to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './dist/output.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-700 w-auto h-screen p-10">
+      <div className="shadow-md justify-center">
+        <h1 className='text-xl text-white text-center font-bold bg-slate-600 rounded-t-md p-2'>
+          Essentials in NLP Project
+        </h1>
+        <div className='p-3 bg-white rounded-b-md'>
+          <p className='text-slate-800 text-xs uppercase font-bold'>
+            Contributors
+          </p>
+          <p className='text-slate-800 text-sm mt-2'>
+            Joel Rüttimann, Tarek Alakmeh, Amos Calamida
+          </p>
+        </div>
+
+      </div>
     </div>
   );
 }
