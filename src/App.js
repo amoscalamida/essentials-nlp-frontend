03e1b42f30e4bb04669e3c3861f8bd7d65656542
Amos Calamida <amos.calamida@me.com>
2023-10-29 13:37:19 +0000

    Add copyright was shown check to disable animation

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ function App() {
               <button onClick={showCopyRightOnMobile} className='inline md:hidden p-0 text-black rounded-full'>
                 
                 <span class="relative flex h-4 w-4">
-                  <span className="absolute animate-ping inline-flex h-full w-full rounded-full bg-slate-400 opacity-75"></span>
+                  <span className={`absolute ${copyrightWasShown ? '' : 'animate-ping'} inline-flex h-full w-full rounded-full bg-slate-400 opacity-75`}></span>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="relative inline-flex h-4 w-4">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                 </svg>
